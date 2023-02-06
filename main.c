3f3e08abd720d5521c9562850bb5d914d5407067
Antonio <antonio-emilio_6@hotmail.com>
2023-02-06 12:55:59 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TAD.H"
+int main()
+{
+    char a[] = "hsabdfkasndafeahweijabiufhsjfnaskjefkashfkjnasbkjdfasdhkjasdfnkjsadbfkashdfkjbsdkhfgawuehsdhflkjsheiruhaiuse";
+    char p[] = "feahweij";
+    forca_bruta(a,p);
+    search(a,p);
+    return 0;
+}
